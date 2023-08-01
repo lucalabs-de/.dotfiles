c731242685d84589f7e8fa315c37b7de6150aa34
lucalabs-de <gspitzer@leoapp-slg.de>
2023-08-02 01:20:39 +0200

    add install script framework

---
+++ .install/install-dotfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Void Linux install script for my dotfiles (WORK IN PROGRESS)
+
+## TODO
+
+# Setup Neovim
+
+. setup-neovim.sh
---
+++ .install/setup-neovim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# install neovim
+xbps-install neovim -y
+
+# install dependencies for neovim features & extensions
+xpbs-install ag xclip -y

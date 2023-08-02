f9f8fcac74b80c1b4e3ab17044638233a11bd6f1
lucalabs-de <gspitzer@leoapp-slg.de>
2023-08-02 13:59:13 +0200

    add neogit to nvim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -8,5 +8,6 @@ return require('packer').startup(function(use)
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use { 'NeogitOrg/neogit', requires = 'nvim-lua/plenary.nvim' }
   use 'windwp/nvim-autopairs'
 end)

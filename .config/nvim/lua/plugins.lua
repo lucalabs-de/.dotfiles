e235940cc768fa0d1ef6252f5bbce9afbccc04e8
lucalabs-de <gspitzer@leoapp-slg.de>
2023-08-03 20:51:47 +0200

    improve nvim config

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,18 +1,16 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'neanias/everforest-nvim'
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
-  use { 'NeogitOrg/neogit', requires = 'nvim-lua/plenary.nvim' }
-  use 'windwp/nvim-autopairs'
-  use 'folke/which-key.nvim'
-  use {
-	  'nvim-lualine/lualine.nvim',
-	  requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  } 
+	use 'wbthomason/packer.nvim'
+	use 'neanias/everforest-nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.2',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+	use { 'NeogitOrg/neogit', requires = 'nvim-lua/plenary.nvim' }
+	use 'windwp/nvim-autopairs'
+	use 'folke/which-key.nvim'
+	use 'nvim-lualine/lualine.nvim'
+	use 'goolord/alpha-nvim'
 end)

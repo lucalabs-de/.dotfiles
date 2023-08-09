307210a6a6fbc6dfbd1e4eedd0b488aa8d6a6555
lucalabs-de <gspitzer@leoapp-slg.de>
2023-08-09 02:03:27 +0200

    add nvim function to quickly change working dir

--- .config/nvim/lua/funcs.lua
+++ .config/nvim/lua/funcs.lua
@@ -1,7 +1,6 @@
 function SetWorkingDir()
   vim.cmd('cd %:h') -- changes working directory to location of current file
-  local ok, git_dir = pcall(vim.cmd, '!git rev-parse --show-toplevel') -- gets git root dir
-  if ok then
-    vim.cmd(string.format('cd %s', git_dir)) -- change working dir to git root dir
-  end
+  local git_dir = vim.fn.system("git rev-parse --show-toplevel")
+  vim.cmd(string.format('cd %s', git_dir))
 end
+

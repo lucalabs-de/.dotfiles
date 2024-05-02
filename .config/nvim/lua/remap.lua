657944ea82500e1af1c9d013335f1066d044bd0c
lucalabs-de <gspitzer@leoapp-slg.de>
2024-05-03 01:36:23 +0200

    update vim keybinds

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -16,8 +16,9 @@ vim.keymap.set("n", "<C-p>", "<Plug>(YankyCycleBackward)")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<leader>bb", "<C-6>")
+vim.keymap.set("n", "<C-J>", "gj")
+vim.keymap.set("n", "<C-K>", "gk")
 
 -- Convenient editing
-vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")

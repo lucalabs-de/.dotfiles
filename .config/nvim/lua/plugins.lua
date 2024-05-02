dff61676dede5ec962aeac47bea33b61657be369
lucalabs-de <gspitzer@leoapp-slg.de>
2024-05-03 01:42:31 +0200

    fix indents

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,17 +1,17 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use 'neanias/everforest-nvim'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.2',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+  use 'wbthomason/packer.nvim'
+  use 'neanias/everforest-nvim'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use 'nvim-treesitter/playground'
-	use { 'NeogitOrg/neogit', requires = 'nvim-lua/plenary.nvim' }
-	use 'nvim-lualine/lualine.nvim'
-	use 'goolord/alpha-nvim'
+  use { 'NeogitOrg/neogit', requires = 'nvim-lua/plenary.nvim' }
+  use 'nvim-lualine/lualine.nvim'
+  use 'goolord/alpha-nvim'
 
   -- Productivity
   use 'gbprod/yanky.nvim'
@@ -38,6 +38,3 @@ return require('packer').startup(function(use)
 
   use "numToStr/Comment.nvim"
 end)
-
--- TODO
--- snippet setup
